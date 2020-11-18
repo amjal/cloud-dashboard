1822742ab11f5d67b54fbef6fac09734a7aa57be
mahdi <mahdi1376ghanbari@gmail.com>
2020-11-18 21:04:51 +0330

    add comments

--- Cloud1/views.py
+++ Cloud1/views.py
@@ -4,10 +4,11 @@ import re
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-info_of_vms = {}
 
+# function for getting data from VBoxManage
 
 def get_vms():
+    # command for getting list of vms
     os.system('VBoxManage list -l vms >> text.txt')
     file1 = open('text.txt', 'r')
     lines = file1.readlines()
@@ -39,6 +40,7 @@ def get_vms():
     return out
 
 
+# main view of our website that show all vms
 def dashboard(request):
     out_vms = {}
     vms = get_vms()
@@ -54,36 +56,44 @@ def dashboard(request):
     return render(request, 'index.html', {'vms': out_vms})
 
 
+# view for changing status of vm
+
 def change_status(request):
     vm_name = request.POST.get('vm')
     new_state = request.POST.get('new_state')
     command = None
+    # check if new state is off
     if new_state == 'off':
+        # command for turning off vm
         command = 'VBoxManage controlvm  ' + vm_name + ' poweroff'
+    # check if new state is on
     elif new_state == 'on':
+        # command for turning on vm
         command = 'VBoxManage startvm ' + vm_name + ' --type headless'
     if command is not None:
         os.system(command)
     return HttpResponse('status changed')
 
 
+# view for cloning vm
 def clone(request):
     vm = request.GET.get('vm')
     vm_name = request.GET.get('vm_name')
     if vm is not None and vm_name is not None:
+        # command for cloning vm
         command = 'VBoxManage clonevm ' + vm + ' --name=' + vm_name + ' --register --mode=all '
         os.system(command)
-    # TODO in code et naghese. avalan bayad state vm ha avaz she va in beheshoon ezafe she. baadesham inke bayad yejoori
-    # be front dade beshe ke beshe tooye oon list in vm e jadid namayesh dade beshe
     return redirect('dashboard')
 
 
+# view for changing config of vm
 def change_config(request):
     vm_name = request.POST.get('vm')
     cores = request.POST.get('core-numbers')
     ram = request.POST.get('ram-space')
     response = ""
     if vm_name is not None:
+        # making command of changing vm config
         command = 'VBoxManage modifyvm ' + vm_name
         if ram is not None:
             command = command + ' --memory ' + ram
@@ -95,15 +105,17 @@ def change_config(request):
     return HttpResponse(response)
 
 
+# command for removing vm
 def remove(request):
     vm_name = request.POST.get('vm')
     if vm_name is not None:
+        # command for remove vm
         command = 'VBoxManage unregistervm ' + vm_name + ' --delete'
         os.system(command)
-    # TODO inja ham list e vm ha update nemishe!
     return redirect('dashboard')
 
 
+# view for running command in vms
 def command(request):
     user_command = request.POST.get('command')
     vm = request.POST.get('vm')
@@ -111,6 +123,7 @@ def command(request):
     password = request.POST.get('password')
     user_command_parts = user_command.split()
     command = None
+    # making command for running command
     if len(user_command_parts) > 0:
         main_command = user_command_parts[0]
         command = 'VBoxManage guestcontrol ' + vm + ' run --exe /bin/' + main_command + ' ' + \
